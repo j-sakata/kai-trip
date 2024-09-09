608aca0f99cd10444af8dfdf4944075c1e9f085d
Joji Sakata <helloworld01joji25@gmail.com>
2024-09-09 21:20:49 +0900

    use kaitabi link

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import chromedriver_binary
 
 def main():
     driver = webdriver.Chrome()
-    driver.get('https://www.google.co.jp')
+    driver.get('https://hoshinoresorts.com/jp/sp/kaitabi20s/')
     
 if __name__ == "__main__":
     main()
